c45572c8f6d38a27cae1a41a23b4bb101074ec44
sherkhan7 <9669470@gmail.com>
2021-05-10 12:12:47 +0500

    add delete_message_by_message_id() update globalvariables.py

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,5 +1,7 @@
-from DB import get_user
+from telegram import TelegramError
 
+from DB import get_user
+from globalvariables import *
 
 def set_user_data(user_id, user_data):
     value = user_data.setdefault('user_data', None)
@@ -18,3 +20,18 @@ def wrap_tags(*args):
     symbol = ' ' if len(args) > 1 else ''
 
     return f'<b><i><u>{symbol.join(args)}</u></i></b>'
+
+
+def delete_message_by_message_id(context, user):
+    user_data = context.user_data
+
+    if MESSAGE_ID in user_data:
+        try:
+            context.bot.delete_message(user[TG_ID], user_data[MESSAGE_ID])
+        except TelegramError:
+            try:
+                context.bot.edit_message_reply_markup(user[TG_ID], user_data[MESSAGE_ID])
+            except TelegramError:
+                pass
+        finally:
+            user_data.pop(MESSAGE_ID)
