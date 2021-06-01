e5db5f402bd26a22f91c132ddb19030105e50d23
sherkhan7 <9669470@gmail.com>
2021-06-02 01:07:21 +0500

    XlsxWriter package added import_database() added

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,4 +1,10 @@
+import xlsxwriter
+import datetime
+
 from telegram import TelegramError
+
+from DB import *
+from config import XLSXFILES_PATH
 from globalvariables import *
 
 
@@ -20,3 +26,38 @@ def delete_message_by_message_id(context, user):
                 pass
         finally:
             user_data.pop(MESSAGE_ID)
+
+
+def import_database():
+    # Workbook() takes one, non-optional, argument
+    # which is the filename that we want to create.
+    document_name = datetime.datetime.now().strftime("users_and_orders_%d-%m-%Y_%H-%M-%S") + '.xlsx'
+    workbook = xlsxwriter.Workbook(document_name)
+    bold = workbook.add_format({'bold': True})
+
+    # The workbook object is then used to add new
+    # users_worksheet via the add_worksheet() method.
+    users_worksheet = workbook.add_worksheet('users')
+
+    fields_list = {"A1": "Ismi", "B1": "Telegram username", "C1": "Foydalanuvchi ID", "D1": "Ro'yxatdan o'tgan sana"}
+    # Writing some data headers
+    for key, value in fields_list.items():
+        users_worksheet.write(key, value, bold)
+    users_worksheet.set_column('A:D', 25)
+    # Start from the first cell below the headers.
+    row = 1
+
+    # Use the users_worksheet object to write
+    # data via the write() method.
+
+    for user in get_all_users():
+        selected_fields = [user[FULLNAME], user[USERNAME], user[ID], user['created_at'].strftime("%d-%m-%Y %X")]
+        for col in range(len(selected_fields)):
+            if selected_fields[col] is None:
+                selected_fields[col] = 'None'
+            users_worksheet.write(row, col, selected_fields[col])
+        row += 1
+    workbook.close()
+
+
+import_database()
