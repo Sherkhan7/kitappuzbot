84decc8b076fe16ce69303d503a3859130d24c8b
sherkhan7 <9669470@gmail.com>
2021-06-12 23:45:11 +0500

    major update - edi bot added

--- handlers/messagehandler.py
+++ handlers/messagehandler.py
@@ -10,6 +10,7 @@ from config import *
 from layouts import *
 from helpers import wrap_tags, import_database
 
+from replykeyboards import ReplyKeyboard
 from replykeyboards.replykeyboardvariables import *
 from replykeyboards.replykeyboardtypes import reply_keyboard_types
 
@@ -30,16 +31,18 @@ def message_handler_callback(update: Update, context: CallbackContext):
         my_orders_btn = reply_keyboard_types[client_menu_keyboard]['my_orders_btn'][f'text_{user[LANG]}']
         social_nets_btn = reply_keyboard_types[client_menu_keyboard]['social_nets_btn'][f'text_{user[LANG]}']
         contact_us_btn = reply_keyboard_types[client_menu_keyboard]['contact_us_btn'][f'text_{user[LANG]}']
+        edit_bot_btn = reply_keyboard_types[admin_menu_keyboard]['edit_bot_btn'][f'text_{user[LANG]}']
+        back_btn = reply_keyboard_types[edit_bot_keyboard]['back_btn'][f'text_{user[LANG]}']
 
         if user[IS_ADMIN]:
             # Yangi buyurtmalar
             if re.search(f'{new_orders_btn}$', update.message.text):
                 waiting_orders = get_orders_by_status(status='waiting')
+
                 if waiting_orders:
                     for order in waiting_orders:
                         order_items = get_order_items_book_title(order[ID])
                         client = get_user(order[USER_ID])
-                        geo = ujson.loads(order[GEOLOCATION]) if order[GEOLOCATION] else None
                         new_dict = dict()
                         label = '[Yangi buyurtma]'
 
@@ -47,26 +50,20 @@ def message_handler_callback(update: Update, context: CallbackContext):
                             new_dict.update({item['book_id']: item['quantity']})
 
                         text_for_admin = f'🆔 {order[ID]} '
-
-                        if order['with_action']:
-                            text_for_admin += get_action_layout(new_dict)
-                        else:
-                            text_for_admin += get_basket_layout(new_dict, user[LANG], data=label)
-
+                        text_for_admin += get_basket_layout(new_dict, user[LANG], data=label)
                         text_for_admin += f'\nMijoz: {wrap_tags(client[FULLNAME])}\n' \
                                           f'Tel: {wrap_tags(order[PHONE_NUMBER])}\n'
-                        # f'Manzil: {wrap_tags(order[ADDRESS])}\n'
                         text_for_admin += f'Telegram: {wrap_tags("@" + client[USERNAME])}\n' \
                             if client[USERNAME] else '\n'
                         text_for_admin += f'Status: {wrap_tags("qabul qilish kutilmoqda")}'
-                        inline_keyboard = InlineKeyboard(orders_keyboard, user[LANG], [geo, order[ID]]).get_keyboard()
+                        inline_keyboard = InlineKeyboard(orders_keyboard, user[LANG], order[ID]).get_keyboard()
                         update.message.reply_text(text_for_admin, reply_markup=inline_keyboard)
 
                 else:
                     update.message.reply_text('Yangi buyurtmalar mavjud emas !')
                 return
 
-                # Qabul qilingan buyutmalar or Tarix
+            # Qabul qilingan buyurtmalar va Tarrix
             elif re.search(f'({received_orders_btn}|{history_btn})$', update.message.text):
                 if re.search(f'{history_btn}$', update.message.text):
                     orders = get_orders_by_status(('delivered', 'canceled'))
@@ -83,8 +80,6 @@ def message_handler_callback(update: Update, context: CallbackContext):
                     wanted = 1
                     order = orders[wanted - 1]
                     client = get_user(order[USER_ID])
-                    if order['with_action']:
-                        label += "[🔥MEGA AKSIYA🔥]"
                     status = 'yetkazilgan' if order[STATUS] == 'delivered' else 'rad etilgan' \
                         if order[STATUS] == 'canceled' else 'qabul qilingan'
                     order_items = get_order_items_book_title(order[ID])
@@ -92,12 +87,6 @@ def message_handler_callback(update: Update, context: CallbackContext):
                     inline_keyboard = InlineKeyboard(paginate_keyboard, user[LANG], data=[wanted, orders],
                                                      history=history).get_keyboard()
                     update.message.reply_text(books_layout, reply_markup=inline_keyboard)
-                    # if order[GEOLOCATION]:
-                    #     geo = ujson.loads(order[GEOLOCATION])
-                    #     inline_keyboard = inline_keyboard.inline_keyboard
-                    #     keyboard = InlineKeyboard(geo_keyboard, data=geo).get_keyboard().inline_keyboard
-                    #     inline_keyboard += keyboard
-                    #     inline_keyboard = InlineKeyboardMarkup(inline_keyboard)
 
                 else:
                     update.message.reply_text(empty_text)
@@ -109,6 +98,19 @@ def message_handler_callback(update: Update, context: CallbackContext):
                 with open(import_database(), 'rb') as file:
                     update.message.reply_document(file)
                 return
+
+            # Botni tahrirlash
+            elif re.search(f'{edit_bot_btn}$', update.message.text):
+                reply_keyboard = ReplyKeyboard(edit_bot_keyboard, user[LANG]).get_keyboard()
+                update.message.reply_text(update.message.text, reply_markup=reply_keyboard)
+                return
+
+            # « Ortga
+            elif re.search(f'{back_btn}$', update.message.text):
+                reply_keyboard = ReplyKeyboard(admin_menu_keyboard, user[LANG]).get_keyboard()
+                update.message.reply_text(update.message.text, reply_markup=reply_keyboard)
+                return
+
             else:
                 thinking_emoji = '🤔🤔'
                 update.message.reply_text(thinking_emoji, quote=True)
@@ -124,9 +126,6 @@ def message_handler_callback(update: Update, context: CallbackContext):
                     order = user_orders[wanted - 1]
                     order_items = get_order_items_book_title(order[ID])
                     label = ''
-                    if order['with_action']:
-                        label += "[🔥MEGA AKSIYA🔥]"
-
                     status = 'qabul qilingan' if order[STATUS] == 'received' else 'rad etilgan' \
                         if order[STATUS] == 'canceled' else 'qabul qilish kutilmoqda' \
                         if order[STATUS] == 'waiting' else 'yetkazilgan'
@@ -153,9 +152,9 @@ def message_handler_callback(update: Update, context: CallbackContext):
 
             # Biz bilan bo'glanish
             elif re.search(f'{contact_us_btn}$', update.message.text):
-                text = f"Kitapp premium adminlari bilan boglanish uchun:\n" \
-                       f"{wrap_tags('@kitapp_admin', '[ +998999131099 ]')} ga murojaat qilishingiz mumkin."
-                update.message.reply_text(text)
+                contact_us_text = get_contact_us_text()
+                contact_us_text = contact_us_text['text'] if contact_us_text else update.message.text
+                update.message.reply_text(contact_us_text)
                 return
 
             else:
@@ -165,7 +164,6 @@ def message_handler_callback(update: Update, context: CallbackContext):
 
     else:
         reply_text = "🔴 Siz ro'yxatdan o'tmagansiz !\nBuning uchun /start ni bosing"
-        # "\U000026A0 Сиз рўйхатдан ўтмагансиз !\nБунинг учун /start ни босинг"
         update.message.reply_text(reply_text)
         return
 
