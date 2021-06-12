22b3ae3c0221005794c4785eec3b57425ed9cd33
sherkhan7 <9669470@gmail.com>
2021-06-12 23:48:49 +0500

    - edit_books_conversation_handler and edit_contactus_conversation_handler added

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,4 +4,5 @@ from .registrationconversation import registration_conversation_handler
 from .booksconversation import books_conversation_handler
 from .commandhandler import command_handler
 from .sendpostconversation import sendpost_conversation_handler
-from .editcontacusconversation import edit_contact_us_conversation_handler
+from .editcontacusconversation import edit_contactus_conversation_handler
+from .editbooksconversation import edit_books_conversation_handler
