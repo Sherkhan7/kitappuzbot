2eb8b7d2055926246d045a6a026862dcbc2fa6cd
sherkhan7 <9669470@gmail.com>
2021-06-12 23:37:06 +0500

    major changes

--- DB/main.py
+++ DB/main.py
@@ -23,32 +23,23 @@ def insert_data(data, table_name):
     data_values = tuple(data.values())
     fields = ','.join(data_keys)
     mask = ','.join(['%s'] * len(data_values))
-
     with closing(get_connection()) as connection:
         with connection.cursor() as cursor:
             sql = f'INSERT INTO {table_name} ({fields}) VALUES ({mask})'
             cursor.execute(sql, data_values)
             connection.commit()
-
     print(f'{table_name}: +{cursor.rowcount}(last_row_id = {cursor.lastrowid})')
     return cursor.lastrowid
 
 
-def insert_order_items(items_data, table_name):
-    basket = items_data.pop('basket')
-    data_keys = list(items_data.keys())
-    data_keys += ['book_id', 'quantity']
-    data_values = tuple(items_data.values())
-    data_values = [data_values + tuple([book_id, quantity]) for (book_id, quantity) in basket.items()]
-    fields = ','.join(data_keys)
-    mask = ','.join(['%s'] * len(data_keys))
-
+def insert_order_items(data_values, fields_list, table_name):
+    fields = ','.join(fields_list)
+    mask = ','.join(['%s'] * len(fields_list))
     with closing(get_connection()) as connection:
         with connection.cursor() as cursor:
             sql = f'INSERT INTO {table_name} ({fields}) VALUES ({mask})'
             cursor.executemany(sql, data_values)
             connection.commit()
-
     print(f'{table_name}: +{cursor.rowcount}')
     return cursor.rowcount
 
@@ -63,14 +54,29 @@ def get_user(_id):
 def get_book(_id):
     with closing(get_connection()) as connection:
         with connection.cursor() as cursor:
+            cursor.execute(f'SELECT * FROM {books_table_name} WHERE id = %s AND is_removed = false', _id)
+    return cursor.fetchone()
+
+
+def get_book_data(_id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
             cursor.execute(f'SELECT * FROM {books_table_name} WHERE id = %s', _id)
     return cursor.fetchone()
+
+
+def delete_book(_id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('UPDATE `books` SET is_removed = true WHERE id = %s ', _id)
+            connection.commit()
+    return 'updated' if connection.affected_rows() != 0 else 'not updated'
 
 
 def get_all_books():
     with closing(get_connection()) as connection:
         with connection.cursor() as cursor:
-            cursor.execute(f'SELECT * FROM {books_table_name}')
+            cursor.execute(f'SELECT * FROM {books_table_name} WHERE is_removed = false')
     return cursor.fetchall()
 
 
@@ -88,12 +94,11 @@ def get_all_orders():
     return cursor.fetchall()
 
 
-def get_books(ids):
-    interval = ",".join(ids)
-
+def get_books(interval):
     with closing(get_connection()) as connection:
         with connection.cursor() as cursor:
-            cursor.execute(f'SELECT * FROM {books_table_name} WHERE id in ({interval}) ORDER BY id DESC')
+            cursor.execute(f'SELECT * FROM {books_table_name} WHERE id in ({interval}) AND is_removed = FALSE '
+                           f'ORDER BY id DESC')
     return cursor.fetchall()
 
 
@@ -172,5 +177,85 @@ def update_user_info(_id, **kwargs):
     with closing(get_connection()) as connection:
         with connection.cursor() as cursor:
             cursor.execute(sql, (value, _id, _id))
+            connection.commit()
+    return 'updated' if connection.affected_rows() != 0 else 'not updated'
+
+
+def update_book_photo(file_id, _id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('UPDATE `books` SET photo = %s WHERE id = %s', (file_id, _id))
+            connection.commit()
+    return 'updated' if connection.affected_rows() != 0 else 'not updated'
+
+
+def update_book_title(title, _id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('UPDATE `books` SET title = %s WHERE id = %s', (title, _id))
+            connection.commit()
+    return 'updated' if connection.affected_rows() != 0 else 'not updated'
+
+
+def update_book_price(price, _id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('UPDATE `books` SET price = %s WHERE id = %s', (price, _id))
+            connection.commit()
+    return 'updated' if connection.affected_rows() != 0 else 'not updated'
+
+
+def update_book_lang(lang, _id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('UPDATE `books` SET lang = %s WHERE id = %s', (lang, _id))
+            connection.commit()
+    return 'updated' if connection.affected_rows() != 0 else 'not updated'
+
+
+def update_book_translator(translator, _id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('UPDATE `books` SET translator = %s WHERE id = %s', (translator, _id))
+            connection.commit()
+    return 'updated' if connection.affected_rows() != 0 else 'not updated'
+
+
+def update_book_cover(cover_type, _id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('UPDATE `books` SET cover_type = %s WHERE id = %s', (cover_type, _id))
+            connection.commit()
+    return 'updated' if connection.affected_rows() != 0 else 'not updated'
+
+
+def update_book_url(description_url, _id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('UPDATE `books` SET description_url = %s WHERE id = %s', (description_url, _id))
+            connection.commit()
+    return 'updated' if connection.affected_rows() != 0 else 'not updated'
+
+
+def update_book_amount(amount, _id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('UPDATE `books` SET amount = %s WHERE id = %s', (amount, _id))
+            connection.commit()
+    return 'updated' if connection.affected_rows() != 0 else 'not updated'
+
+
+def update_book_year(year, _id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('UPDATE `books` SET year = %s WHERE id = %s', (year, _id))
+            connection.commit()
+    return 'updated' if connection.affected_rows() != 0 else 'not updated'
+
+
+def update_book_author(author, _id):
+    with closing(get_connection()) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('UPDATE `books` SET author = %s WHERE id = %s', (author, _id))
             connection.commit()
     return 'updated' if connection.affected_rows() != 0 else 'not updated'
